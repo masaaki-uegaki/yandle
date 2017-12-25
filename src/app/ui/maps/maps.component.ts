35da87edfb3e2e747a5afdf682dc6ed6c52f678d
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2017-12-26 07:50:42 +0900

    Mod map

--- src/app/ui/maps/maps.component.ts
+++ src/app/ui/maps/maps.component.ts
@@ -2,27 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { MapService } from '../../service/map/map.service';
 import { Location } from '../../model/location';
 import { Router, ActivatedRoute } from '@angular/router';
+import _ from "lodash";
 
 @Component({
-  selector: 'app-maps',
-  templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.scss']
+    selector: 'app-maps',
+    templateUrl: './maps.component.html',
+    styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent implements OnInit {
-  location: Location = {lat: 35.839404, lng: 137.202668};
-  scrollwheel: boolean = true;
-  zoom = 11;
+    location: Location = {lat: 35.839404, lng: 137.202668};
+    scrollwheel: boolean = true;
+    zoom = 11;
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private mapService: MapService) {
+    constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private mapService: MapService) {
+      }
+
+    ngOnInit() {
+      this.setLocation();
     }
 
-  ngOnInit() {
-      this.mapService.getCurrentLocation()
-          .subscribe((location: Location) => {
-              this.location = location;
-          });
-  }
+    setLocation() {
+        this.route.queryParams.subscribe((params)=> {
+            if (params.lat && params.lng) {
+                this.location = {lat: Number.parseFloat(params.lat), lng: Number.parseFloat(params.lng)};
+            } else {
+                this.mapService.getCurrentLocation()
+                    .subscribe((location: Location) => {
+                        this.location = location;
+                    });
+            }
+        });
+    }
 }
