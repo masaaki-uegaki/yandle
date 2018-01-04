78e37ec5f289ed7eb95f3a4c64d26bca1bc05585
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-01-04 18:17:22 +0900

    Remove remove garbage codes

--- src/app/service/yell/yell.service.ts
+++ src/app/service/yell/yell.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import * as moment from 'moment'; 
-import * as firebase from 'firebase';
 
 import { AngularFirestore } from 'angularfire2/firestore';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { Yell } from '../../model/yell';
 
